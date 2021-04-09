5b2ad4f084e8dd7ed4521fb8c168a561beaa3f0e
cybermin <sillamin@naver.com>
2021-04-09 09:40:56 +0900

    Delete opencv.py

--- opencv.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import cv2
-
-image = cv2.imread("C:/Users/user/AppData/Local/Programs/Python/images/moon.jpg", cv2.IMREAD_UNCHANGED)
-cv2.imshow("Moon", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
